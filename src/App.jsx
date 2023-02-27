5bd0fe413e45d5ad0096a0efb7926a2a6678ea56
fahimehsd <fahimehsadeghi3@gmail.com>
2023-02-27 16:02:27 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MainPage from "./components/mainPage/MainPage";
+import "./index.css";
+import { createBrowserRouter, Route, Routes } from "react-router-dom";
+const App = () => {
+  return (
+    <createBrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+      <MainPage />
+    </createBrowserRouter>
+  );
+};
+
+export default App;
