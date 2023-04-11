f8c358d04937adda4aae2a5f2c2df0a32c1f9815
fahimehsd <fahimehsadeghi3@gmail.com>
2023-04-11 09:00:16 +0330

    search done

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Search from "../search/Search";
 
 const Header = () => {
   // useEffect(() => {
@@ -68,29 +69,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="flex items-center">
-          <div className="relative ml-2 ">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block placeholder-gray-500 w-[11rem] bg-transparent pl-8 border-2 border-fuchsia-900 text-fuchsia-900 rounded-lg text-xs"
-              placeholder="what are you looking for?"
-            />
-          </div>
+          <Search />
           <Link to={"/auth"} className="decoration-transparent">
             <span className=" text-gray-500">
               <h3 className="m-4">Management</h3>
